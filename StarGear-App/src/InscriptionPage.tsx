db7ad8f3c05de373cea5e62fc6c9864edc05c736
Felix Allard <2475742@bdeb.qc.ca>
2026-02-26 17:23:49 -0500

    Ajout de la page d'inscription

--- StarGear-App/src/InscriptionPage.tsx
+++ StarGear-App/src/InscriptionPage.tsx
@@ -1,50 +1,101 @@
 import { Link } from "react-router";
 
-export default function SinscrirePage() {
+export default function InscriptionPage() {
   return (
-
     <div>
-       <nav className="navbar navbar-expand-lg stargear-navbar px-4 py-3">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">
-          <img src="/src/img/starGear.png"  style={{marginLeft:"6rem", marginTop:"0.2rem"}} alt="StarGear" height="150" width="auto"  className="logo position-absolute  translate-middle   " />
-        </a>
-        <div className="collapse navbar-collapse" id="navMenu">
-          <ul className="navbar-nav mx-auto text-center gap-lg-4">
-            <li className="nav-item">
-              <a className="nav-link nav-custom " href="">
-                Accueil
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link nav-custom" href="">
-                Boutique
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link nav-custom" href="">
-                À propos
-              </a>
-            </li>
-          </ul>
-
-          <div className="d-flex justify-content-center justify-content-lg-end">
-            <a href="" className="btn btn-login">
-              Se connecter
-            </a>
+      <nav className="navbar navbar-expand-lg stargear-navbar px-4 py-3">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            <img
+              src="/src/img/starGear.png"
+              style={{ marginLeft: "6rem", marginTop: "0.2rem" }}
+              alt="StarGear"
+              height="150"
+              width="auto"
+              className="logo position-absolute  translate-middle   "
+            />
+          </a>
+          <div className="collapse navbar-collapse" id="navMenu">
+            <ul className="navbar-nav mx-auto text-center gap-lg-4">
+              <li className="nav-item">
+                <a className="nav-link nav-custom text-white " href="">
+                  Accueil
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link nav-custom text-white" href="">
+                  Boutique
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link nav-custom text-white" href="">
+                  À propos
+                </a>
+              </li>
+            </ul>
+            <div className="d-flex justify-content-center justify-content-lg-end">
+              <button
+                type="submit"
+                className=" rounded-5 btn btn-danger btn-block "
+              >
+                Se connecter
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
-    <div>
-      <h1>S'inscrire</h1>
-      <Link to="/SeConnecterPage" className="btn btn-primary">
-        Go to form
-      </Link>
-      <br></br>
-    </div>
+      </nav>
 
+      <div className="pt-5 d-flex justify-content-end">
+        <form
+          className="mt-5 pt-3 rounded-4 ps-4 pe-4 container"
+          style={{
+            width: "400px",
+            height: "400px",
+            backgroundColor: "#1a1a1a",
+          }}
+        >
+          <h3 className="text-danger text-center fw-semibold pb-4">
+            Inscription
+          </h3>
+          <p className="text-secondary ">
+            {" "}
+            En vous inscrivant, vous acceptez les{" "}
+            <a href="/conditions" style={{ textDecoration: "underline" }}>
+              Conditions d’utilisation
+            </a>{" "}
+            et la{" "}
+            <a href="/confidentialite" style={{ textDecoration: "underline" }}>
+              Politique de confidentialité
+            </a>
+            {" "}
+          </p>
+          <div className="mb-3">
+            <div className="text-secondary">Courriel</div>
+            <input
+              type="email"
+              className="form-control bg-dark text-white"
+              id="inputEmail"
+            />
+            <div id="emailHelp" className="form-text"></div>
+          </div>
+          <div className="pt-3 pb-4 text-center d-grid gap-2 col-9 mx-auto">
+            <button
+              type="submit"
+              className=" rounded-5 btn btn-danger btn-block "
+            >
+              Créer un compte{" "}
+            </button>
+          </div>
+          <div>
+            <p className="text-secondary text-center pt-2">
+              Vous avez déjà un compte ?{" "}
+              <a href="/SeConnecterPage" className="text-danger">
+                Connectez-vous
+              </a>
+            </p>
+          </div>
+        </form>
+      </div>
     </div>
-    
   );
 }
