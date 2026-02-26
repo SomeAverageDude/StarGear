d6531077e914303a0b1851e516523103d29cc244
Felix Allard <2475742@bdeb.qc.ca>
2026-02-26 14:48:11 -0500

    Ajout de la navbar sur les deux pages

--- StarGear-App/src/InscriptionPage.tsx
+++ StarGear-App/src/InscriptionPage.tsx
@@ -4,6 +4,38 @@ export default function SinscrirePage() {
   return (
 
     <div>
+       <nav className="navbar navbar-expand-lg stargear-navbar px-4 py-3">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          <img src="/src/img/starGear.png"  style={{marginLeft:"6rem", marginTop:"0.2rem"}} alt="StarGear" height="150" width="auto"  className="logo position-absolute  translate-middle   " />
+        </a>
+        <div className="collapse navbar-collapse" id="navMenu">
+          <ul className="navbar-nav mx-auto text-center gap-lg-4">
+            <li className="nav-item">
+              <a className="nav-link nav-custom " href="">
+                Accueil
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link nav-custom" href="">
+                Boutique
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link nav-custom" href="">
+                À propos
+              </a>
+            </li>
+          </ul>
+
+          <div className="d-flex justify-content-center justify-content-lg-end">
+            <a href="" className="btn btn-login">
+              Se connecter
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
     <div>
       <h1>S'inscrire</h1>
       <Link to="/SeConnecterPage" className="btn btn-primary">
--- StarGear-App/src/SeConnecterPage.tsx
+++ StarGear-App/src/SeConnecterPage.tsx
@@ -4,13 +4,57 @@ export default function SeConnecterPage() {
   return (
 
     <div>
+
+    <nav className="navbar navbar-expand-lg stargear-navbar px-4 py-3">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          <img src="/src/img/starGear.png"  style={{marginLeft:"6rem", marginTop:"0.2rem"}} alt="StarGear" height="150" width="auto"  className="logo position-absolute  translate-middle   " />
+        </a>
+
+
+        <div className="collapse navbar-collapse" id="navMenu">
+          <ul className="navbar-nav mx-auto text-center gap-lg-4">
+            <li className="nav-item">
+              <a className="nav-link nav-custom " href="">
+                Accueil
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link nav-custom" href="">
+                Boutique
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link nav-custom" href="">
+                À propos
+              </a>
+            </li>
+          </ul>
+
+          <div className="d-flex justify-content-center justify-content-lg-end">
+            <a href="" className="btn btn-login">
+              Se connecter
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
+
     <div>
-      <h1>Se Connecter</h1>
-      <Link to="/SeConnecterPage" className="btn btn-primary">
-        Go to form
-      </Link>
+      <Link to="/InscriptionPage" className="btn btn-primary">      </Link>
       <br></br>
     </div>
+
+
+
+
+
+
+
+
+
+
+
 
     </div>
   
