6414562f4843f5bda1e9cb1387c32be70476d862
arrisyannis <arrisy0@gmail.com>
2026-02-27 15:05:36 -0500

    ajout de css pour le navBar

--- react-tutorial/src/HomePage.tsx
+++ react-tutorial/src/HomePage.tsx
@@ -1,39 +1,36 @@
-import { Link } from "react-router";
+import "./HomePage.css"
 
 export default function HomePage() {
   return (
     
        <nav className="navbar navbar-expand-lg stargear-navbar px-4 py-3">
-      <div className="container-fluid">
-      
-       
+      <div className="container-fluid">      
 
         <a className="navbar-brand" href="#">
           <img src="/src/img/starGear.png"  style={{marginLeft:"6rem", marginTop:"0.2rem"}} alt="StarGear" height="150" width="auto"  className="logo position-absolute  translate-middle   " />
         </a>
 
-        {/* Links + Button */}
         <div className="collapse navbar-collapse" id="navMenu">
           <ul className="navbar-nav mx-auto text-center gap-lg-4">
             <li className="nav-item">
-              <a className="nav-link nav-custom active" href="#">
+              <a className="nav-link nav-custom " href="">
                 Accueil
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link nav-custom" href="#">
+              <a className="nav-link nav-custom" href="">
                 Boutique
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link nav-custom" href="#">
+              <a className="nav-link nav-custom" href="">
                 À propos
               </a>
             </li>
           </ul>
 
           <div className="d-flex justify-content-center justify-content-lg-end">
-            <a href="#" className="btn btn-login">
+            <a href="" className="btn btn-login">
               Se connecter
             </a>
           </div>
@@ -41,9 +38,5 @@ export default function HomePage() {
       </div>
     </nav>
 
-
-
-
-    
   );
 }
