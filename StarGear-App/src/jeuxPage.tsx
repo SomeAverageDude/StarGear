cf708ecf626a789d1d92f125cb1cb840dc0c0095
SomeAverageDude <mtour999998@gmail.com>
2026-02-27 08:37:53 -0500

    added nav bar

--- StarGear-App/src/jeuxPage.tsx
+++ StarGear-App/src/jeuxPage.tsx
@@ -2,17 +2,48 @@ import { Link } from "react-router";
 
 export default function jeuxPage() {
   return (
-
     <div>
-    <div>
-      <h1>Jeux</h1>
-      <Link to="/App" className="btn btn-primary">
-        Go to form
-      </Link>
-      <br></br>
-    </div>
+      <nav className="navbar navbar-expand-lg stargear-navbar px-4 py-3">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            <img
+              src="/src/assets/starGear.png"
+              style={{ marginLeft: "6rem", marginTop: "0.2rem" }}
+              alt="StarGear"
+              height="150"
+              width="auto"
+              className="logo position-absolute  translate-middle   "
+            />
+          </a>
 
-    </div>
+          <div className="collapse navbar-collapse" id="navMenu">
+            <ul className="navbar-nav mx-auto text-center gap-lg-4">
+              <li className="nav-item">
+                <a className="nav-link nav-custom " href="">
+                  Accueil
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link nav-custom" href="">
+                  Boutique
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link nav-custom" href="">
+                  À propos
+                </a>
+              </li>
+            </ul>
 
+            <div className="d-flex justify-content-center justify-content-lg-end">
+              <a href="" className="btn btn-login">
+                Se connecter
+              </a>
+            </div>
+          </div>
+        </div>
+      </nav>
+      
+    </div>
   );
 }
--- StarGear-App/src/main.tsx
+++ StarGear-App/src/main.tsx
@@ -6,10 +6,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import App from "./App.tsx";
 import HomePage from "./HomePage.tsx";
+import JeuxPage from "./jeuxPage.tsx";
 
 const router = createBrowserRouter([
   { path: "/", element: <HomePage /> },
   { path: "/App", element: <App /> },
+  { path: "/Jeu", element: <JeuxPage /> },
 ]);
 
 createRoot(document.getElementById("root")!).render(
