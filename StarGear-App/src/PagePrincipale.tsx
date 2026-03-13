5a53e30fbf8b18512e4ac43a66f64bfd50243c58
Felix Allard <2475742@bdeb.qc.ca>
2026-03-13 10:47:15 -0400

    Ajout de la communication avec le serveur

--- StarGear-App/src/PagePrincipale.tsx
+++ StarGear-App/src/PagePrincipale.tsx
@@ -1,11 +1,34 @@
+import { useEffect, useState } from "react";
+
+type Jeu = {
+    id: number;
+    nom_jeu: string;
+    description: string;
+    prix: number;
+    image_url: string;
+    date_de_sortie: Date;
+};
+
+
 export default function PagePrincipale() {
-    const style = {
+  const style = {};
+  const [jeux, setJeux] = useState<Jeu[]>([]);
 
-    }
+  useEffect(() => {
+    fetch("http://localhost:4000/jeux")
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Failed to fetch events");
+        }
+        return res.json();
+      })
+      .then((data) => setJeux(data))
+      .catch((err) => console.error(err));
+  }, []);
 
-
-  return <div style={style}>
-     <nav className="navbar navbar-expand-lg stargear-navbar px-4 py-3">
+  return (
+    <div style={style}>
+      <nav className="navbar navbar-expand-lg stargear-navbar px-4 py-3">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">
             <img
@@ -47,5 +70,25 @@ export default function PagePrincipale() {
         </div>
       </nav>
 
-  </div>;
+    <div className="list-group">
+            {jeux.map((jeu) => (
+            <div key={jeu.id} className="list-group-item">
+                <div className="row align-items-center">
+                {/* Game Info */}
+                <div className="col-8">
+                    <h5 className="mb-1">{jeu.nom_jeu}</h5>
+                    <p className="mb-1">{jeu.description}</p>
+                    <p className="mb-1">Prix: {jeu.prix} $</p>
+                    <p className="mb-1">Année de sortie: {new Date(jeu.date_de_sortie).getFullYear()}</p>
+                    <small className="text-muted">{jeu.image_url}</small>
+                </div>
+                </div>
+                </div>
+            ))}</div>
+                
+
+
+    </div>
+  );
 }
+function gameCard() {}
