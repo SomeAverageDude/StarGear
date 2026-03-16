c10e3fdfcec45316450fcaf39ff21076c23a35bf
Luc797 <cielj455@gmail.com>
2026-03-15 21:27:19 -0400

    Completion de la page admin

--- StarGear-App/serveur/server.ts
+++ StarGear-App/serveur/server.ts
@@ -19,3 +19,71 @@ const pool = mysql.createPool({
     database: "StarGear",
 });
 
+app.get("/jeux", async (req, res) => {
+    try {
+        const [rows] = await pool.query("SELECT * FROM jeux");
+        res.status(200).json(rows);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message: "Database error"})
+    }
+});
+
+app.get("/jeux/:id", async (req, res) => {
+    try {
+        const id = Number(req.params.id);
+
+        const [result] = await pool.query("SELECT * FROM jeux WHERE id_jeu = ?", [
+            id,
+        ]);
+
+        const jeu = (result as any[])[0];
+
+        if (!jeu){
+            return res.status(404).json({ message: "Jeu non trouvé" });
+        }
+
+        res.status(200).json(result);
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Database error" });
+    }
+});
+
+app.put("/jeux/:id/edit", async (req, res) => {
+    try {
+        const id = Number(req.params.id);
+        const {nom_jeu,developpeur,date_de_sortie,prix,sale,description,file_size,revue_id_revue} = req.body;
+
+        const result = await pool.query(`UPDATE jeux
+            SET nom_jeu = ?, developpeur = ?, date_de_sortie = ?, prix = ?, sale = ?, description = ?, file_size = ?, revue_id_revue = ?
+            WHERE id_jeu = ?`,
+            [nom_jeu,developpeur,date_de_sortie,prix,sale,description,file_size,revue_id_revue,id],
+        );
+
+        res.status(200).json({message: "Jeu modifié"})
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Database error" });
+    }
+});
+
+app.delete("/jeux/:id/delete", async (req,res) => {
+    try {
+        const id = Number(req.params.id);
+
+        await pool.query("DELETE FROM bibliotheque WHERE jeux_id_jeu = ?", [id]);
+        
+        const result = await pool.query("DELETE FROM jeux where id_jeu = ?",[id]);
+
+        res.status(204).send();
+    } catch (error){
+        console.error(error);
+        res.status(500).json({ message: "Database error" });
+    }
+});
+
+
+
+
--- StarGear-App/src/main.tsx
+++ StarGear-App/src/main.tsx
@@ -8,12 +8,14 @@ import App from "./App.tsx";
 import SeConnecterPage from "./SeConnecterPage.tsx";
 import InscriptionPage from "./InscriptionPage.tsx";
 import HomePage from "./HomePage.tsx";
+import AdminPage from "./AdminPage.tsx";
 
 const router = createBrowserRouter([
   { path: "/", element: <HomePage /> },
   { path: "/App", element: <App /> },
   { path: "/SeConnecterPage", element: <SeConnecterPage /> },
   { path: "/InscriptionPage", element: <InscriptionPage /> },
+  { path: "/AdminPage", element: <AdminPage /> },
 ]);
 
 createRoot(document.getElementById("root")!).render(
