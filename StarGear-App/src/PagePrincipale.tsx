4fba55cb500d81efe0e3abb17fbb1786ebcf1a7b
Felix Allard <2475742@bdeb.qc.ca>
2026-03-16 22:07:42 -0400

    Ajout navbar dans toutes les pages

--- StarGear-App/src/PagePrincipale.tsx
+++ StarGear-App/src/PagePrincipale.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
+import Navbar from "./helper/navbar";
 
 type Jeu = {
   id_jeu: number;
@@ -31,47 +32,7 @@ export default function PagePrincipale() {
 
   return (
     <div className="bg-black text-white min-vh-100">
-      <nav className="navbar navbar-expand-lg stargear-navbar px-4 py-3">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            <img
-              src="/src/assets/starGear.png"
-              style={{ marginLeft: "6rem", marginTop: "0.2rem" }}
-              alt="StarGear"
-              height="150"
-              width="auto"
-              className="logo position-absolute translate-middle"
-            />
-          </a>
-          <div className="collapse navbar-collapse" id="navMenu">
-            <ul className="navbar-nav mx-auto text-center gap-lg-4">
-              <li className="nav-item">
-                <a className="nav-link nav-custom text-white" href="/">
-                  Accueil
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link nav-custom text-white" href="">
-                  Boutique
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link nav-custom text-white" href="">
-                  À propos
-                </a>
-              </li>
-            </ul>
-            <div className="d-flex justify-content-center justify-content-lg-end">
-              <button
-                type="submit"
-                className="rounded-5 btn btn-danger btn-block"
-              >
-                Se connecter
-              </button>
-            </div>
-          </div>
-        </div>
-      </nav>
+     <Navbar></Navbar>
 
       <section
         className="position-relative d-flex align-items-center justify-content-center"
---
+++ StarGear-App/src/helper/navbar.tsx
@@ -0,0 +1,43 @@
+import { useNavigate } from "react-router";
+
+export default function Navbar() {
+  const navigate = useNavigate();
+
+  return (
+    <nav className="navbar navbar-expand-lg stargear-navbar px-4 py-3 bg-dark">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          <img
+            src="/src/assets/starGear.png"
+            style={{ marginLeft: "6rem", marginTop: "0.2rem" }}
+            alt="StarGear"
+            height="150"
+            width="auto"
+            className="logo position-absolute translate-middle"
+          />
+        </a>
+        <div className="collapse navbar-collapse" id="navMenu">
+          <ul className="navbar-nav mx-auto text-center gap-lg-4">
+            <li className="nav-item">
+              <a className="nav-link nav-custom text-white" href="/">Accueil</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link nav-custom text-white" href="">Boutique</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link nav-custom text-white" href="">À propos</a>
+            </li>
+          </ul>
+          <div className="d-flex justify-content-center justify-content-lg-end">
+            <button
+              className="rounded-5 btn btn-danger btn-block"
+              onClick={() => navigate("/SeConnecterPage")}
+            >
+              Se connecter
+            </button>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
