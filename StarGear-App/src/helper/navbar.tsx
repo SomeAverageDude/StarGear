3de7f3feb3276ecf9bf3240d1fad564b6f930e46
SomeAverageDude <mtour999998@gmail.com>
2026-03-27 10:58:40 -0400

    it verks

--- StarGear-App/src/helper/navbar.tsx
+++ StarGear-App/src/helper/navbar.tsx
@@ -11,7 +11,7 @@ export default function Navbar() {
             src="/src/assets/starGear.png"
             style={{ marginLeft: "6rem", marginTop: "0.2rem" }}
             alt="StarGear"
-            height="150"
+            height="50px"
             width="auto"
             className="logo position-absolute translate-middle"
           />
@@ -19,13 +19,22 @@ export default function Navbar() {
         <div className="collapse navbar-collapse" id="navMenu">
           <ul className="navbar-nav mx-auto text-center gap-lg-4">
             <li className="nav-item">
-              <a className="nav-link nav-custom text-white" href="/">Accueil</a>
+              <a className="nav-link nav-custom text-white" href="/">
+                Accueil
+              </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link nav-custom text-white" href="/PagePrincipale">Boutique</a>
+              <a
+                className="nav-link nav-custom text-white"
+                href="/PagePrincipale"
+              >
+                Boutique
+              </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link nav-custom text-white" href="/">À propos</a>
+              <a className="nav-link nav-custom text-white" href="/">
+                À propos
+              </a>
             </li>
           </ul>
           <div className="d-flex justify-content-center justify-content-lg-end">
