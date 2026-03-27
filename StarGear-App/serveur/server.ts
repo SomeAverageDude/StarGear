212eb18f041ab14ddea55314d5df8e86da49d7b8
Felix Allard <2475742@bdeb.qc.ca>
2026-03-27 08:49:19 -0400

    Ajout de la route de la recherche

--- StarGear-App/serveur/server.ts
+++ StarGear-App/serveur/server.ts
@@ -138,3 +138,20 @@ app.get("/images/:id", async (req, res) => {
     }
 });
 
+
+app.get("/jeux/search", async (req,res) => {
+    try{
+        const nom_jeu = (req.query.nom_jeu);
+           if (!nom_jeu) {
+            return res.status(400).json({ message: "Nom du jeu requis" });
+        }
+
+        const result = await pool.query("SELECT * FROM jeux where nom_jeux LIKE ?", [`%${nom_jeu}%`]);
+        
+        res.status(200).json({result});
+    } catch (error){
+        console.error(error);
+        res.status(500).json({message: "Database error"})
+    }
+});
+
