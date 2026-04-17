9968ddcf7c116b719e29f8edaf0428912b6e4b4d
Felix Allard <2475742@bdeb.qc.ca>
2026-04-17 10:32:45 -0400

    Creation de la page panier

---
+++ StarGear-App/src/PanierPage.tsx
@@ -0,0 +1,18 @@
+import Navbar from "./helper/navbar";
+import Footer from "./helper/footer";
+
+
+
+
+export default function PanierPage() {
+  return (
+    <div className="bg-black text-white min-vh-100">
+      <Navbar />
+      <h1 className="text-center ">Panier</h1>
+
+    
+
+      <Footer />
+    </div>
+  );
+}
