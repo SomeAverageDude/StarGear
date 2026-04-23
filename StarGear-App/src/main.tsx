d82aa1df98c2692e1d7ecf2d1f54482576c06072
Felix Allard <2475742@bdeb.qc.ca>
2026-04-23 17:04:26 -0400

    Fix merge-conflicts (branch api-igdb)

--- StarGear-App/src/main.tsx
+++ StarGear-App/src/main.tsx
@@ -1,9 +1,7 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import { ToastContainer } from "react-toastify";
 import SeConnecterPage from "./SeConnecterPage.tsx";
 import InscriptionPage from "./InscriptionPage.tsx";
 import HomePage from "./HomePage.tsx";
@@ -11,6 +9,11 @@ import JeuxPage from "./jeuxPage.tsx";
 import PagePrincipale from "./PagePrincipale.tsx";
 import AdminPage from "./AdminPage.tsx";
 import AproposPage from "./AproposPage.tsx";
+
+
+import { ToastContainer } from "react-toastify";
+
+
 
 const router = createBrowserRouter([
   { path: "/", element: <HomePage /> },
@@ -23,8 +26,9 @@ const router = createBrowserRouter([
 ]);
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <RouterProvider router={router}></RouterProvider>
-     <ToastContainer position="top-right" autoClose={3000} theme="dark" />
-  </StrictMode>,
+  <> 
+     <RouterProvider router={router}></RouterProvider>,
+     <ToastContainer position="top-right" autoClose={3000} theme="dark"/>
+     </>
+    
 );
