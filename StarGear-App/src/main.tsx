ba09bb9771d75154848138681c8bb0f3e3f54aaa
SomeAverageDude <mtour999998@gmail.com>
2026-05-07 15:58:57 -0400

    removed white space at end of pages

--- StarGear-App/src/main.tsx
+++ StarGear-App/src/main.tsx
@@ -27,8 +27,8 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById("root")!).render(
   <> 
-     <RouterProvider router={router}></RouterProvider>,
+     <RouterProvider router={router}></RouterProvider>
      <ToastContainer position="top-right" autoClose={3000} theme="dark"/>
-     </>
+  </>
     
 );
