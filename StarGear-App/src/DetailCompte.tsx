8c32b1032819794ea0204eb4ccfe0ec47d486344
Felix Allard <2475742@bdeb.qc.ca>
2026-05-08 10:50:45 -0400

    Creation de la page de detail

---
+++ StarGear-App/src/DetailCompte.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+type User = {
+  _id: string;
+  courriel: string;
+  nomUtilisateur: string;
+};
+
+
+export default function detailCompte() {
+  const [user, setUser] = useState<User | null>(null);
+
+
+
+    return (
+        <div> 
+            
+            
+            
+            
+            
+              </div>
+
+
+    )
+
+
+}
